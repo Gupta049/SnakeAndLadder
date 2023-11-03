3fcb47ec983e8f865c69975cda93eb3b087bc6fa
“Gupta049” <sushilgupta049@gmail.com>
2023-11-03 09:27:48 +0530

    Modified : Some changes in code

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -7,7 +7,7 @@ public class SnakeAndLadder {
     static int playerPosition = 0;
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game");
-        System.out.println("Initial position : " + playerPosition);
+        System.out.println(" Initial position : " + playerPosition);
 
         while (playerPosition < WIN_POSITION) {
             int diceValue = (int) (Math.random() * 10) % 6 + 1;
