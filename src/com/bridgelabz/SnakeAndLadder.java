ca684dd0ab6d059c80eabd78e122974619784a10
“Gupta049” <sushilgupta049@gmail.com>
2023-11-03 09:21:39 +0530

    Added : Winning Position Set

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,37 +1,46 @@
 package com.bridgelabz;
 
+import sun.awt.Win32ColorModel24;
+
 public class SnakeAndLadder {
+    static final int WIN_POSITION = 100;
     static int playerPosition = 0;
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game");
         System.out.println("Initial position : " + playerPosition);
 
-        int diceValue = (int)(Math.random()*10)%6+1;
-        System.out.println("Dice Value = " + diceValue);
+        while (playerPosition < WIN_POSITION) {
+            int diceValue = (int) (Math.random() * 10) % 6 + 1;
+            System.out.println("Dice Value = " + diceValue);
 
-        int randomOption = (int)(Math.random()*10%3); // generating random number for checking player position
-        System.out.println("Choice = " + randomOption);
+            // generating random number for checking player position
+            int randomOption = (int) (Math.random() * 10 % 3);
+            System.out.println("Choice = " + randomOption);
 
-        switch (randomOption) {
-            case 0:
-                System.out.println("No Play");
-                break;
-            case 1:
-                //ladder
-                playerPosition = playerPosition + diceValue;
-                System.out.println(playerPosition);
-                break;
-            case 2:
-                //snake
-                playerPosition = playerPosition - diceValue;
-                if(playerPosition < 0) {
+            switch (randomOption) {
+                case 0:
+                    System.out.println("No Play");
+                    break;
+                case 1:
+                    //ladder
                     playerPosition = playerPosition + diceValue;
-                }
-                System.out.println(playerPosition);
-                break;
-            default:
-                System.out.println("Invalid");
-                break;
+                    if(playerPosition > WIN_POSITION){
+                        playerPosition = playerPosition - diceValue;
+                    }
+                    System.out.println(playerPosition);
+                    break;
+                case 2:
+                    //snake
+                    playerPosition = playerPosition - diceValue;
+                    if (playerPosition < 0) {
+                        playerPosition = 0;
+                    }
+                    System.out.println(playerPosition);
+                    break;
+                default:
+                    System.out.println("Invalid");
+                    break;
+            }
         }
     }
 }
